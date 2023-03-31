46795c4c02252a9828cf1e0f9a1db6e0fb9627d1
Duoc Vo <duoc.vo@kyanon.digital>
2023-03-31 15:36:26 +0700

    init card gym project

---
+++ card-gym/card-gym/CardVC.swift
@@ -0,0 +1,16 @@
+//
+//  CardVC.swift
+//  card-gym
+//
+//  Created by DuocVo on 3/31/23.
+//
+
+import UIKit
+
+class CardVC: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .brown
+    }
+}
---
+++ card-gym/card-gym/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  card-gym
+//
+//  Created by DuocVo on 3/31/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
