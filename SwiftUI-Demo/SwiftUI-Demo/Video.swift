97496f3ece92b046cbb4b3b904457dcce16c41c6
Duoc Vo <duoc.vo@kyanon.digital>
2023-04-03 15:30:05 +0700

    demo swift ui

---
+++ SwiftUI-Demo/SwiftUI-Demo/Video.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  SwiftUI-Demo
+//
+//  Created by DuocVo on 4/3/23.
+//
+
+import Foundation
+
+struct Video {
+  var imageName: String
+  var title: String
+  var uploadDate: 
+}
